ee9445d6050537be413c196a41e8dfb831c11990
yasheyxs <jazmingaidoyxs@gmail.com>
2026-01-29 16:59:06 -0300

    commit

--- BACK-END/api/cors.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
